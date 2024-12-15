1c343a833fe229ee257840dfa05bf83da945bcc3
전민오 <jbj338033@gmail.com>
2024-12-16 08:29:31 +0900

    feat: pagination type

---
+++ apps/web/src/types/pagination.ts
@@ -0,0 +1,28 @@
+export type Sort = {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+};
+
+export type Pageable = {
+  pageNumber: number;
+  pageSize: number;
+  sort: Sort;
+  offset: number;
+  paged: boolean;
+  unpaged: boolean;
+};
+
+export type Page<T> = {
+  content: T[];
+  pageable: Pageable;
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  first: boolean;
+  size: number;
+  number: number;
+  sort: Sort;
+  numberOfElements: number;
+  empty: boolean;
+};
